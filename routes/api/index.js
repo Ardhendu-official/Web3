7683562756e7d5205fcebec2112cb0cc2dac6db7
Ardhendu-official <ardhendudasofficial@gmail.com>
2022-11-09 01:40:24 +0530

    Create Account & Wallet

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,11 +5,13 @@ const router = express.Router();
 // const recordingsRoutes = require('./recordings');
 // const statusRoutes = require('./status');
 const home = require('./home');
+const ether = require('./ether');
 
 // router.use('/cameras', cameraRoutes);
 // router.use('/recordings', recordingsRoutes);
 // router.use('/status', statusRoutes);
 
 router.use('/', home)
+router.use('/ether', ether)
 
 module.exports = router;
