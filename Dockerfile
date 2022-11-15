437fff66d0c95c012ebdd83849b3a8521fdae183
Ardhendu-official <ardhendudasofficial@gmail.com>
2022-11-15 16:21:57 +0530

    Import Wallet

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,19 @@
+FROM node:16.17.1-buster
 # create a directory for the node app
 WORKDIR /app
 
 # upgrade packages
-RUN apt-get update && apt-get upgrade -y
+# RUN apt-get update && apt-get upgrade -y
+
 # install nodejs
-RUN curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-RUN apt-get update && apt-get install -y nodejs
+# RUN curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+# RUN apt-get update && apt-get install -y nodejs
+
 # install yarn
-RUN npm install -g yarn
+# RUN npm install -g yarn
 
 
-COPY src/package.json /app
-RUN yarn install
-COPY src/ /app
-CMD ['node', 'app.js']
+COPY package.json /app
+RUN npm install
+COPY ./ /app
+CMD ["node", "app.js"]
--- routes/api/tron.js
+++ routes/api/tron.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const Web3 = require("web3");
 const TronWeb = require('tronweb');
+const hdWallet = require('tron-wallet-hd');
+const keyStore=hdWallet.keyStore;
+const utils=hdWallet.utils;
 const axios = require("axios")
 const fullNode = 'https://api.shasta.trongrid.io';
 const solidityNode = 'https://api.shasta.trongrid.io';
@@ -17,30 +20,100 @@ router.get("/", async (req, res) => {
 
 router.post("/account", async (req, res) => {
     let data = await tronWeb.createAccount();
-    res.send(data);
+    let value = await utils.generateMnemonic();
+    // let seed = utils.generateMnemonic();
+    // let accounts = await utils.generateAccountsWithMnemonic(seed,1);
+    res.send([data, value]);
+
 })
 
 router.post("/wallet", async (req, res) => {
     let data = await tronWeb.createRandom({ path: "m/44'/195'/0'/0/0", extraEntropy: '', locale: 'en' });
-    res.send(data);
+    let final_data = {
+        "mnemonic": {
+          "phrase": data.mnemonic.phrase,
+          "path": "m/44'/195'/0'/0/0",
+          "locale": "en"
+        },
+        "privateKey": tronWeb.fromUtf8(data.privateKey),
+        "publicKey": tronWeb.toAscii(data.publicKey),
+        "privateey": tronWeb.toUtf8(tronWeb.fromUtf8(data.privateKey)),
+        "publicey": tronWeb.fromAscii(tronWeb.toAscii(data.publicKey)),
+        "address": data.address
+    }
+    res.send([final_data, data]);
 })
 
 router.post("/wallet/details", async (req, res) => {
     let url = `https://apilist.tronscan.org/api/account?address=${req.body.address}`;
 
     if(tronWeb.isAddress(req.body.address)){
-        tronWeb.trx.getBalance(req.body.address).then((result) => {
+        tronWeb.trx.getAccount(req.body.address).then((result) => {
             let data = result
             console.log(data);
-            res.send(JSON.parse(data));
+            res.send(data)
         })
     }
 })
 
 router.post("/wallet/import", async (req, res) => {
+    // let pKey = req.body.pkey;
+    // let data = await tronWeb.address.fromPrivateKey(pKey)
     let mKey = req.body.mkey;
     let data = await tronWeb.fromMnemonic(mKey)
     res.send(data);
 })
+
+router.post("/wallet/import/private", async (req, res) => {
+    let pKey = req.body.pkey;
+    let key = await tronWeb.address.toHex(pKey)
+    let data = await tronWeb.address.fromPrivateKey(key);
+    // let data = await utils.getAccountFromPrivateKey(pKey);
+    res.send(data);
+})
+
+router.post("/wallet/details", async (req, res) => {
+    let url = `https://apilist.tronscan.org/api/account?address=${req.body.address}`;
+    // let url = `https://apilist.tronscan.org/api/account?address=${req.body.address}`;
+
+    if(tronWeb.isAddress(req.body.address)){
+        tronWeb.trx.getAccount(req.body.address).then((result) => {
+            let data = result
+            console.log(data);
+            res.send(data)
+        })
+    }
+})
+
+router.post("/wallet/send", async (req, res) => {
+    // const privateKey = "0x6acf29c540a0c29899c898e6c31a7d318a97dfcba0148760c951f51d9dbc9dfb"; 
+    // var fromAddress = "TGNFDPZimQ33UiQvjF6MH7dZdJ2dWpUncb"; //address _from
+    // var toAddress = "TRs3FRfbN9cV8NY53TBqCRcytRMyw53r7p"; //address _to
+    // var amount = 10; //amount
+    // //Creates an unsigned TRX transfer transaction
+    // tradeobj = await tronWeb.transactionBuilder.sendTrx(
+    //     toAddress,
+    //     amount,
+    //     fromAddress
+    // );
+    // let signedtxn = await tronWeb.trx.sign(
+    //     tradeobj,
+    //     privateKey
+    // );
+    // let receipt = await tronWeb.trx.sendRawTransaction(
+    //     signedtxn
+    // ).then(output => {console.log('- Output:', output, '\n');});
+    let data = await tronWeb.transactionBuilder.sendTrx("TRs3FRfbN9cV8NY53TBqCRcytRMyw53r7p", 100, "TGNFDPZimQ33UiQvjF6MH7dZdJ2dWpUncb");
+    res.send(data)
+})
+
+router.post("/wallet/balance", async (req, res) => {
+    // const privateKey = "6db106d7432c4bf7dbaad30959d66b6aacfa4664bd9511375a8c71ea829bdee1"; 
+    let address = "TGNFDPZimQ33UiQvjF6MH7dZdJ2dWpUncb";  
+    //Get account information，Query the trx balance by the balance in the return value.
+    tradeobj = await tronWeb.trx.getBalance(address) .then(output => {console.log('- Output:', output, '\n');});
+    res.send(tradeobj)
+})
+
 
 module.exports = router;
